6f02707fb12104b5fc8aeeb8fa8e1d980ffda514
St4ckkk <keyanandydelgado@gmail.com>
2024-10-28 13:18:21 +0800

    haha

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -4,25 +4,25 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
+  if (authHeader) {
+
+    const token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) return res.status(403).json({ message: "Invalid Token" });
+      req.user = user;
+      next();
+    });
+  } else {
     return res.status(401).json({ message: "You are not authenticated" });
+
   }
 
-  const token = authHeader.split(" ")[1];
-
-  jwt.verify(token, process.env.JWT_SEC, (err, user) => {
-    if (err) {
-      return res.status(403).json({ message: "Invalid Token" });
-    }
-
-    req.user = user;
-    next();
-  });
 };
 
 const verifyAndAuthorization = (req, res, next) => {
   verifyToken(req, res, () => {
-    if (req.user.id) {
+    if (req.user.id || req.user.isAdmin) {
       next();
     } else {
       res.status(403).json({ message: "You are not allowed to do that!" });
