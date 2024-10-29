82f46a19963338dfdbcafa44e2b5d6e1d5b31599
St4ckkk <keyanandydelgado@gmail.com>
2024-10-29 18:21:03 +0800

    test

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const bodyParser = require('body-parser')
 dotenv.config();
 
 const admin = require('firebase-admin');
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = require('./FIREBASE_SERVICE_ACCOUNT/jobility-c1d00-firebase-adminsdk-jm7dq-4ada377134.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
