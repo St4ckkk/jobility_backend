9f3279dedbf3830d6b813bcc8f2bb886fa72d403
K <96453690+St4ckkk@users.noreply.github.com>
2024-10-29 23:53:00 +0800

    Update User.js

--- models/User.js
+++ models/User.js
@@ -20,6 +20,10 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    uid: {
+      type: String,
+      required: true
+    },
     location: {
       type: String,
       required: false,
